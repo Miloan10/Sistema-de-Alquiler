96824768e75f24936bc5eccfe7fbe86a49f7423a
danny06javier-c <danny06javier@gmail.com>
2026-09-17 22:45:49 -0500

    ACTUALIZACION

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Vehiculo> vehiculos = new ArrayList<>();
 
-        vehiculos.add(new Auto("Ferrari", "Pura Sangre", 80));
+        vehiculos.add(new Auto("Ferrari", "SF90", 100));
         vehiculos.add(new Moto("BMW", "M1000RR", 50));
 
         int dias = 5;
